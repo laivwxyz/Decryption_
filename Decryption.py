6177cc1d754237046934ec1ce8a4841e4136387f
laivwxyz <delacruzlailanie2004@gmail.com>
2023-04-16 21:50:05 +0800

    pseudocode has been added

--- Decryption.py
+++ Decryption.py
@@ -1,3 +1,12 @@
 # Dela Cruz, Lailanie E. _ BSCPE 1-4
 # Assignment 2
 # Problem 2 - Decryption
+
+# Ask the user to input an encrypted text
+# Check every character
+# if *, change to a
+# if &, change to e
+# if #, change to i
+# if +, change to o
+# if !, change to u
+# print the output
